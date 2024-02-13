3d1ea9284dbe2c6320ab05ce4c7135e96977b07b
Nayeel Imtiaz <nayeelimtiaz11@gmail.com>
2024-02-12 21:35:00 -0800

    Pushing hello.swift

---
+++ hello.swift
@@ -0,0 +1,10 @@
+// import Foundation
+
+print("Hello, World!")
+
+let fruits = ["apple", "banana", "cookie"]
+
+for (index, fruit) in fruits.enumerated() {
+    // print(index, fruit)
+    print("\(index): \(fruit)")
+}
